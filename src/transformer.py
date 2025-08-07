f821cf6aaed2f85fbe20cebcbb0e18d6a39ad66a
Robert He <hnrobert@qq.com>
2025-08-07 18:04:32 +0800

    fix: black formatter, add suggestions

--- src/transformer.py
+++ src/transformer.py
@@ -4,8 +4,12 @@ import string
 from datetime import datetime
 from typing import Any
 
-from models import (JinshanWebhookData, LotteryWebhookPayload, ParticipantInfo,
-                    PowerAutomatePayload)
+from models import (
+    JinshanWebhookData,
+    LotteryWebhookPayload,
+    ParticipantInfo,
+    PowerAutomatePayload,
+)
 
 logger = logging.getLogger(__name__)
 
